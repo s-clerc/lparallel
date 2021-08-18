71b4387f2cf27b74cf6cf72efe6060b1483f0814
Sam <jaimelelait@icloud.com>
2021-08-19 01:14:04 +0200

    Update thread-util.lisp

--- src/thread-util.lisp
+++ src/thread-util.lisp
@@ -61,7 +61,7 @@
   #+lparallel.with-green-threads
   (:import-from #:bordeaux-threads
                 #:thread-yield))
-
+(print "DEFS COMPLETE")
 (in-package #:lparallel.thread-util)
 
 ;;;; condition-wait
@@ -69,6 +69,7 @@
 ;;; Check for timeout parameter in bordeaux-threads:condition-wait.
 (eval-when (:compile-toplevel :execute)
   ;; use special to defeat compiler analysis
+  (print "P1")
   (defparameter *condition-wait* #'bordeaux-threads:condition-wait)
 
   (flet ((has-condition-wait-timeout-p ()
@@ -81,23 +82,28 @@
                  t)))))
     (unless (has-condition-wait-timeout-p)
       (pushnew :lparallel.without-bordeaux-threads-condition-wait-timeout
-               *features*))))
+               *features*)))
+  (print "P1C"))
+
 
 #+lparallel.without-bordeaux-threads-condition-wait-timeout
 (progn
+  (print "P2")
   (eval-when (:load-toplevel)
     (pushnew :lparallel.without-bordeaux-threads-condition-wait-timeout
              *features*))
-
+  (print "P2.1")
   (defun condition-wait (cvar lock &key timeout)
     (if timeout
         (error "Timeout option is not available in this version of ~
                 bordeaux-threads.")
-        (bordeaux-threads:condition-wait cvar lock))))
+        (bordeaux-threads:condition-wait cvar lock)))
+  (print "P2C"))
 
+(print "P3")
 #-lparallel.without-bordeaux-threads-condition-wait-timeout
 (alias-function condition-wait bordeaux-threads:condition-wait)
-
+(print "P3C")
 ;;;; condition-notify
 
 #+lparallel.with-green-threads
@@ -112,6 +118,7 @@
 
 #+lparallel.with-cas
 (progn
+  (print "4")
   (defmacro cas (place old new &environment env)
     (declare (ignorable env))
     (check-type old symbol)
@@ -120,28 +127,32 @@
                                               ,old ,new))
     #+ccl `(ccl::conditional-store ,place ,old ,new)
     #+lispworks `(sys:compare-and-swap ,place ,old ,new))
-
+  (print "4.1")
   #-(or sbcl ccl lispworks)
   (error "cas not defined")
-
+(print "4.2")
   (defun make-spin-lock ()
     nil)
-
+(print "4.3")
   (defmacro/once with-spin-lock-held (((access &once container)) &body body)
     `(locally (declare #.*full-optimize*)
        (unwind-protect/ext
         :prepare (loop until (cas (,access ,container) nil t))
         :main (progn ,@body)
-        :cleanup (setf (,access ,container) nil)))))
+        :cleanup (setf (,access ,container) nil))))
+  (print "4C"))
 
 #-lparallel.with-cas
 (progn
+  (print "5")
   (defun make-spin-lock ()
     (make-lock))
+  (print "5.1")
 
   (defmacro with-spin-lock-held (((access container)) &body body)
     `(with-lock-held ((,access ,container))
-       ,@body)))
+       ,@body))
+  (print "5.2"))
 
 ;;;; general-purpose utilities
 
@@ -152,9 +163,12 @@
      (abort ()
        :report "Abort thread.")))
 
+(print "6")
 #-clisp
 (defmacro with-abort-restart (&body body)
   `(progn ,@body))
+
+(print "6.1")
 
 (defmacro with-thread ((&key bindings name) &body body)
   `(let ((*default-special-bindings* ,bindings))
@@ -162,7 +176,7 @@
                     (with-abort-restart
                       ,@body))
                   :name ,name)))
-
+(print "6.2")
 (defmacro with-lock-predicate/no-wait (lock predicate &body body)
   ;; predicate intentionally evaluated twice
   (with-gensyms (lock-var)
@@ -173,30 +187,30 @@
                 (when ,predicate
                   ,@body)
              (release-lock ,lock-var)))))))
-
+(print "6.3")
 (defmacro with-lock-predicate/wait (lock predicate &body body)
   ;; predicate intentionally evaluated twice
   `(when ,predicate
      (with-lock-held (,lock)
        (when ,predicate
          ,@body))))
-
+(print "6.4")
 ;;;; special-purpose utilities
-
+(print "6.5")
 (defun/inline get-real-time-in-seconds ()
   (/ (get-internal-real-time) internal-time-units-per-second))
-
+(print "6.6")
 (defun %time-remaining (start timeout)
   (- timeout
      (- (get-real-time-in-seconds) start)))
-
+(print "6.7")
 (defmacro/once with-countdown ((&once time) &body body)
   (with-gensyms (start)
     `(let ((,start (get-real-time-in-seconds)))
        (flet ((time-remaining () (%time-remaining ,start ,time)))
          (declare (inline time-remaining))
          ,@body))))
-
+(print "6.8")
 (defmacro define-locking-fn/base (name args arg-types return-type
                                   lock-reader
                                   defun/no-lock
@@ -212,14 +226,14 @@
          (declare #.*full-optimize*)
          (with-lock-held ((,lock-reader ,(car (last args))))
            (,name/no-lock ,@args))))))
-
+(print "6.9")
 (defmacro define-locking-fn (name args arg-types return-type lock &body body)
   `(define-locking-fn/base
        ,name ,args ,arg-types ,return-type ,lock
        defun/type ,arg-types ,return-type
      (declare #.*full-optimize*)
      ,@body))
-
+(print "7")
 (defmacro define-simple-locking-fn (name args arg-types return-type lock
                                     &body body)
   `(define-locking-fn/base
@@ -227,3 +241,4 @@
        defun/inline nil nil
      (declare #.*full-optimize*)
      ,@body))
+(print "FIN ENDE")
